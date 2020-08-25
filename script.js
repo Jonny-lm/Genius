c4a1b51e6ccbc325424b0bf5d3926033fcf4e149
Jonny Lopes <jonny.lopes@nibo.com.br>
2020-08-24 21:31:58 -0300

    melhoria no sorteio de cores

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let order = [];
 let clickedOrder = [];
 let score = 0;
-
-//0 - verde
-//1 - vermelho
-//2 - amarelo
-//3 - azul
+let colorOfNumber = 4;
 
 const blue = document.querySelector('.blue');
 const red = document.querySelector('.red');
@@ -14,7 +10,7 @@ const yellow = document.querySelector('.yellow');
 
 //cria ordem aletoria de cores
 let shuffleOrder = () => {
-    let colorOrder = Math.floor(Math.random() * 4);
+    let colorOrder = Math.floor(Math.random() * colorOfNumber);
     order[order.length] = colorOrder;
     clickedOrder = [];
 
@@ -24,18 +20,33 @@ let shuffleOrder = () => {
     }
 }
 
+//funcao que retorna a cor
+let createColorElement = (color) => {
+    switch (color) {
+        case 0:
+            return green;
+        case 1:
+            return red;
+        case 2:
+            return yellow;
+        case 3:
+            return blue;
+    }
+}
+
 //acende a proxima cor
 let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
         element.classList.add('selected');
+
     }, number - 250);
     setTimeout(() => {
         element.classList.remove('selected');
-    });
+    }, number - 450);
 }
 
-//checa se os botoes clicados sÃ£o os mesmos da ordem gerada no jogo
+//checa se os botoes clicados são os mesmos da ordem gerada no jogo
 let checkOrder = () => {
     for (let i in clickedOrder) {
         if (clickedOrder[i] != order[i]) {
@@ -44,7 +55,7 @@ let checkOrder = () => {
         }
     }
     if (clickedOrder.length == order.length) {
-        alert(`PontuaÃ§Ã£o: ${score}\nVocÃª acertou! Iniciando prÃ³ximo nÃ­vel!`);
+        alert(`pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
         nextLevel();
     }
 }
@@ -60,19 +71,6 @@ let click = (color) => {
     }, 250);
 }
 
-//funcao que retorna a cor
-let createColorElement = (color) => {
-    if (color == 0) {
-        return green;
-    } else if (color == 1) {
-        return red;
-    } else if (color == 2) {
-        return yellow;
-    } else if (color == 3) {
-        return blue;
-    }
-}
-
 //funcao para proximo nivel do jogo
 let nextLevel = () => {
     score++;
@@ -81,7 +79,7 @@ let nextLevel = () => {
 
 //funcao para game over
 let gameOver = () => {
-    alert(`PontuaÃ§Ã£o: ${score}!\nVocÃª perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
     order = [];
     clickedOrder = [];
 
@@ -90,7 +88,7 @@ let gameOver = () => {
 
 //funcao de inicio do jogo
 let playGame = () => {
-    alert('Bem vindo ao GÃªnesis! Iniciando novo jogo!');
+    alert('Bem vindo ao Genesis! Iniciando novo jogo!');
     score = 0;
 
     nextLevel();
